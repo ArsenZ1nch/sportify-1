eac93abd111bd441c808c4e8ec10a3103ff6cf43
Arsenii Zinchenko <70096155+ArsenZ1nch@users.noreply.github.com>
2026-02-11 07:52:06 +0000

    diggi

---
+++ config.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+
+if __name__ == "__main__":
+    try:
+        with open("test.db", "w") as f:
+            f.write("")
+    except Exception:
+        open("test.db", "x")
+
+    database_connection = sqlite3.connect("test.db")
+    database_cursor = database_connection.cursor()
+    database_cursor.execute("CREATE TABLE student(ID int NOT NULL, FirstName varchar(255) NOT NULL, LastName varchar(255) NOT NULL, PRIMARY KEY (ID))")
+    
